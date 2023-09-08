795f0d3d7c56434f58a14c290afed4a71a11ac79
Selçuk AKYOL <121816798+Bayakyol@users.noreply.github.com>
2023-09-08 23:00:17 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import ContactPage from './routes/ContactPage';
+import RecoveryPage from './routes/RecoveryPage';
+import CloudPage from "./routes/CloudPage";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<App/>} />
+  <Route path="/recovery" element={<RecoveryPage/>} />
+  <Route path="/cloud" element={<CloudPage/>} />
+  <Route path="/contact" element={<ContactPage/>} />
+</Routes>
+</BrowserRouter>
+);
+
